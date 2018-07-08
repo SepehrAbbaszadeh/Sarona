d098d104f662b94bdbb449ea90f20d17ad2940c5
sphrtehrani <sphrtehrani@gmail.com>
2018-07-08 13:02:36 +0430

    t

---
+++ Sarona/Models/Numbering.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sarona.Models
+{
+    public class NumberingRange
+    {
+        public long From { get; set; }
+        public long To { get; set; }
+        public byte Min { get; set; }
+        public byte Max { get; set; }
+        public string NumberType { get; set; }
+        public string ChargingCase { get; set; }
+        public string MyProperty { get; set; }
+    }
+}
